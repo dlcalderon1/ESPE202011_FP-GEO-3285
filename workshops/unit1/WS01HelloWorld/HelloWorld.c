665c457f3944103e162e6e440e921284c836df7c
dlcalderon1 <daitha05@gmail.com>
2021-01-08 13:38:13 -0500

    WS01 Hello World in three programing languages

---
+++ workshops/unit1/WS01HelloWorld/HelloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+int main(){
+	int grade=20;
+	//printf() displays information
+	printf("Hello World from \t \" Dayana Calderón! \" =) \n");
+	printf("\n The grade is %d \n", grade);
+	return0;
+}	
+
+ 
---
+++ workshops/unit1/WS01HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+// Dayana Calderón Hello World
+
+/*
+some lines of comments
+can be written in this way
+thanks
+*/
+class HolaMundo {
+
+	public static void main(String[] args) {		
+		System.out.println("Hello World form Dayana Calderón");
+	}
+
+}
