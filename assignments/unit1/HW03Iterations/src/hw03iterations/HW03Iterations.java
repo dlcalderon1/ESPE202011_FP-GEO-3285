3b5e8db278671be442eb1cf3d93d0421bb50e14e
dlcalderon1 <daitha05@gmail.com>
2021-01-14 18:55:54 -0500

    homework 03 iterations

---
+++ assignments/unit1/HW03Iterations/src/hw03iterations/HW03Iterations.java
@@ -0,0 +1,40 @@
+package hw03iterations;
+
+public class HW03Iterations {
+
+    public static void main(String[] args) {
+        int d;
+        d = 0;
+        System.out.println("-------------------------");
+        System.out.println("WHILE LOOP FROM 1 TO 9:");
+        System.out.println("-------------------------");
+        while (d <= 9){
+            System.out.println("     d --> " + d);
+            d+=1;
+        }
+        System.out.println(" ");
+        System.out.println("-------------------------");
+        System.out.println("MULTIPLICATION TABLE: 9");
+        System.out.println("-------------------------");
+        int max=12;
+        int product;
+        int table=9;
+        for (int c = 1; c <= max; c++){
+            product=table * c;
+            System.out.println("     9 * " + c + " = " + product);
+        }
+        System.out.println(" ");
+        System.out.println("-------------------------");
+        System.out.println("POWER TABLE IN BASE: 10");
+        System.out.println("-------------------------");
+        
+        int máx=9;
+        int total;
+        int base=9;
+        for (int min= 0; min <= máx; min++){
+            total = (int) Math.pow(base,(min));
+            System.out.println("     9 ^ " + min + " = " + total);
+        }
+    }
+    
+}
--- workshops/unit1/WS05Iterations/src/ws05iterations/WS05Iterations.java
+++ workshops/unit1/WS05Iterations/src/ws05iterations/WS05Iterations.java
@@ -28,8 +28,8 @@ public class WS05Iterations {
         }
         System.out.println("==== Multiplication Tables : 7 ====");
         int top=12;
-        int product=0;
-        int table=9;
+        int product;
+        int table=7;
         for (int j = 1; j <= top; j++){
             product = table *j;
             System.out.println("7 * " + j + " = " + product);
