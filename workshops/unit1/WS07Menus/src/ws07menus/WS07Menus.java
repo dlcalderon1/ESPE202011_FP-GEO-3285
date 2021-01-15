765b0ca0d44416c95e056afe40fee1838e766e6e
dlcalderon1 <daitha05@gmail.com>
2021-01-15 14:48:57 -0500

    workshop 07 munus

--- workshops/unit1/WS07Menus/src/ws07menus/WS07Menus.java
+++ workshops/unit1/WS07Menus/src/ws07menus/WS07Menus.java
@@ -12,45 +12,44 @@ public class WS07Menus {
         
         do {
             System.out.println("------Calculator------");
-            System.out.println("1. --> Addition");
-            System.out.println("2. --> Subtraction");
-            System.out.println("3. --> Multiplication");
-            System.out.println("4. --> Division");
+            System.out.println("1. --> Division");
+            System.out.println("2. --> Modulus");
+            System.out.println("3. --> Square root ");
+            System.out.println("4. --> Power");
             System.out.println("5. --> Exit");
             
-            System.out.printf("Enter your menu option -->");
+            System.out.printf("Enter your menu option --> " );
             option = input.nextInt();
              switch(option){ //if else sería otra opción
                  case 1:
-                     int addend1;
-                     int addend2;
-                     int sum;
-                     System.out.println("enter addend 1 -->");
-                     addend1 = input.nextInt();
-                     System.out.println("enter addend 2 -->");
-                     addend2 = input.nextInt();
-                     sum = addend1 + addend2;
-                     System.out.println("the sum is -->");
+                     int dividend;
+                     int divisor;
+                     int quotient;
+                     System.out.printf("enter dividend --> ");
+                     dividend = input.nextInt();
+                     System.out.printf("enter divisor --> ");
+                     divisor = input.nextInt();
+                     quotient = dividend / divisor;
+                     System.out.println("the division is --> " + quotient);
                      break;
                  case 2:
-                     System.out.println("The subtraction is");
+                     System.out.println("The modulus is");
                      break;
                  case 3: 
-                     System.out.println("The multiplication is");
+                     System.out.println("The  square root");
                      break;
                  case 4:
-                     System.out.println("The division is");
+                     System.out.println("The power is");
                      break;
                  case 5:
-                     System.out.println("Good bye my friend...");
+                     System.out.println("Good bye friend...");
                      System.exit(0);
                      break;
                  default:
                      System.out.println("Invalid option");
                      break;
-                
              }
-        } while (option < 7);
+        } while (option < 6);
         
        
         }
