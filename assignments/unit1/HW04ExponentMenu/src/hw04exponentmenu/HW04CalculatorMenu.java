b39cd3155b49f503af924d5805a56f975e57bc3a
dlcalderon1 <daitha05@gmail.com>
2021-01-16 17:54:51 -0500

    homework 04 calculator menu

---
+++ assignments/unit1/HW04ExponentMenu/src/hw04exponentmenu/HW04CalculatorMenu.java
@@ -0,0 +1,91 @@
+package hw04exponentmenu;
+
+import java.util.Scanner;
+
+public class HW04ExponentMenu {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        boolean mainLoop = true;
+        int option;
+        
+        do {
+            System.out.println("------Calculator------");
+            System.out.println("1. --> Hypotenuse");
+            System.out.println("2. --> Triangle area");
+            System.out.println("3. --> Square area");
+            System.out.println("4. --> Circle area");
+            System.out.println("5. --> Exit");
+            
+            System.out.printf("Enter your menu option --> " );
+            option = scanner.nextInt();
+             switch(option){
+                 case 1:
+                     float leg1;
+                     float leg2;
+                     float leg11;
+                     float leg22;
+                     float sum;
+                     float hypotenuse;
+                     System.out.printf("enter leg (a) --> ");
+                     leg1 = scanner.nextInt();
+                     System.out.printf("enter leg (b)  --> ");
+                     leg2 = scanner.nextInt();
+                     leg11 = (float) Math.pow(leg1,(2));
+                     leg22 = (float) Math.pow(leg2,(2));
+                     sum = leg11 + leg22;
+                     hypotenuse = (float) Math.sqrt(sum) ;
+                     System.out.println("The hypotenuse is --> " + hypotenuse);
+                     break;
+                 case 2:
+                     float base;
+                     float height;
+                     float total;
+                     System.out.printf("enter base --> ");
+                     base = scanner.nextInt();
+                     System.out.printf("enter height --> ");
+                     height = scanner.nextInt();
+                     total = (base * height)/2;
+                     System.out.println("The triangle area is --> " + total);
+                     break;
+                 case 3:
+                     float side;
+                     float squareSide;
+                     System.out.printf("enter side --> ");
+                     side = scanner.nextInt();
+                     squareSide = (float) Math.pow(side, 2);
+                     System.out.println("The square area is --> " + squareSide);
+                     break;
+                 case 4:
+                     float pi = 3.141592654F;
+                     float radio;
+                     float squareRadio;
+                     float total2;
+                     System.out.printf("enter radio --> ");
+                     radio = scanner.nextInt();
+                     squareRadio = (float) Math.pow(radio, pi);
+                     total2 = radio * squareRadio;
+                     System.out.println("The circle area is --> " +total2);
+                     break;
+                 case 5:
+                     System.out.println("Good bye, COME BACK SOON...");
+                     System.exit(0);
+                     break;
+                 default:
+                     System.out.println("Invalid option");
+                     break;
+             }
+                 
+        } while (option < 6);
+    }
+}
+            
+            
+
+        
+
+    
+       
+    
+    
+
--- workshops/unit1/WS07Menus/src/ws07menus/WS07Menus.java
+++ workshops/unit1/WS07Menus/src/ws07menus/WS07Menus.java
@@ -1,4 +1,3 @@
-
 package ws07menus;
 
 import java.util.Scanner;
@@ -15,16 +14,16 @@ public class WS07Menus {
             System.out.println("1. --> Division");
             System.out.println("2. --> Modulus");
             System.out.println("3. --> Square root ");
-            System.out.println("4. --> Power");
+            System.out.println("4. --> Exponent");
             System.out.println("5. --> Exit");
             
             System.out.printf("Enter your menu option --> " );
             option = input.nextInt();
              switch(option){ //if else sería otra opción
                  case 1:
-                     int dividend;
-                     int divisor;
-                     int quotient;
+                     float dividend;
+                     float divisor;
+                     double quotient;
                      System.out.printf("enter dividend --> ");
                      dividend = input.nextInt();
                      System.out.printf("enter divisor --> ");
@@ -33,13 +32,34 @@ public class WS07Menus {
                      System.out.println("the division is --> " + quotient);
                      break;
                  case 2:
-                     System.out.println("The modulus is");
+                     int dividend2;
+                     int divisor2;
+                     int remainder;
+                     System.out.printf("enter dividend --> ");
+                     dividend2 = input.nextInt();
+                     System.out.printf("enter divisor --> ");
+                     divisor2 = input.nextInt();
+                     remainder = dividend2 % divisor2;
+                     System.out.println("The modulus is --> " + remainder);
                      break;
                  case 3: 
-                     System.out.println("The  square root");
+                     int radicand;
+                     double squareRoot;
+                     System.out.printf("enter radicand --> ");
+                     radicand = input.nextInt();
+                     squareRoot = (float) Math.sqrt(radicand);
+                     System.out.println("The  square root is --> " + squareRoot);
                      break;
                  case 4:
-                     System.out.println("The power is");
+                     int base;
+                     int exponent;
+                     int result; 
+                     System.out.printf("enter base --> ");
+                     base = input.nextInt();
+                     System.out.printf("enter exponent --> ");
+                     exponent = input.nextInt();
+                     result = (int) Math.pow(base,(exponent));
+                     System.out.println("The exponent is --> " + result);
                      break;
                  case 5:
                      System.out.println("Good bye friend...");
