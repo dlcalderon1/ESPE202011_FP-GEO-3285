ff9ec5042c7ed80f5059546f7f5ec611afa9e1ec
dlcalderon1 <daitha05@gmail.com>
2021-02-17 17:26:19 -0500

    workshop 13 Task

--- workshops/unit2/WS13Task/src/ws13task/WS13Task.java
+++ workshops/unit2/WS13Task/src/ws13task/WS13Task.java
@@ -1,35 +1,77 @@
 package ws13task;
+
 import java.util.Scanner;
+
 public class WS13Task {
 
     public static void main(String[] args) {
+        int option;
+
         Scanner data = new Scanner(System.in);
-        System.out.println("RHOMBUS AREA: ");
-        System.out.println("Enter the major diagonal (cm): ");
-        float majorDiagonal = data.nextInt();
-        System.out.println("Enter the minor diagonal (cm): ");
-        float minorDiagonal = data.nextInt();
-        float rhrombusArea;
-        rhrombusArea = computeRhombusArea(majorDiagonal, minorDiagonal);
-        System.out.println("The rhombus area is: " + rhrombusArea + " cm² \n");
-        
-        System.out.println("CIRCLE AREA: ");
-        System.out.println("Enter radio (cm): ");
-        float radio = data.nextInt();
-        float circleArea;
-        circleArea = computeCircleArea(radio);
-        System.out.println("The circle area is: " + circleArea + " cm² \n");
-        
-        System.out.println("CONE AREA: ");
-        System.out.println("Enter radio (cm): ");
-        float radio1 = data.nextInt();
-        System.out.println("Enter the aperture (cm): ");
-        float aperture = data.nextInt();
-        float coneArea;
-        coneArea = computeConeArea(radio, aperture);
-        System.out.println("The circle area is: " + coneArea + " cm² \n");
-        
-        System.out.println("POSITION FORMULA: ");
+        do {
+            System.out.println("------Calculator------");
+            System.out.println("1. --> RHOMBUS AREA");
+            System.out.println("2. --> CIRCLE AREA");
+            System.out.println("3. --> CONE AREA");
+            System.out.println("4. --> POSITION FORMULA");
+            System.out.println("5. --> FORCE FORMULA");
+            System.out.println("6. --> KINETIC ENERGY");
+            System.out.println("7. --> EXIT");
+            
+            System.out.printf("Enter your menu option --> ");
+            option = data.nextInt();
+            switch (option) {
+                case 1:
+                    getRhombusAreaData(data);
+                    break;
+                case 2:
+                    getCircleAreaData(data);
+                    break;
+                case 3:
+                    getConeAreaData(data);
+                    break;
+                case 4:
+                    getPositionFormulaData(data);
+                    break;
+                case 5:
+                    getFordeFormulaData(data);
+                    break;
+                case 6:
+                    getKineticEnergyFormulaData(data);
+                    break;
+                case 7:
+                    System.out.println("Good bye friend...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid option");
+                    break;
+            }
+
+        } while (option < 8);
+    }
+
+    private static void getKineticEnergyFormulaData(Scanner data) {
+        System.out.println("Enter mass (kg): ");
+        float mass1 = data.nextInt();
+        System.out.println("Enter speed (m/s): ");
+        float speed = data.nextInt();
+        float kineticEnergy;
+        kineticEnergy = computeKineticEnergyFormula(mass1, speed);
+        System.out.println("The kinetic energy is: " + kineticEnergy + " J");
+    }
+
+    private static void getFordeFormulaData(Scanner data) {
+        System.out.println("Enter mass (kg): ");
+        float mass = data.nextInt();
+        System.out.println("Enter acceleration (m/s²): ");
+        float acceleration1 = data.nextInt();
+        float force;
+        force = computeForceFormula(mass, acceleration1);
+        System.out.println("The force is: " + force + " N \n");
+    }
+
+    private static void getPositionFormulaData(Scanner data) {
         System.out.println("Enter initial position (m): ");
         float initialPosition = data.nextInt();
         System.out.println("Enter the time (s) : ");
@@ -41,29 +83,35 @@ public class WS13Task {
         float position;
         position = computePositionFormula(initialPosition, time, acceleration, initialSpeed);
         System.out.println("The position is: " + position + " m \n");
-        
-        System.out.println("FORCE FORMULA: ");
-        System.out.println("Enter mass (kg): ");
-        float mass = data.nextInt();
-        System.out.println("Enter acceleration (m/s²): ");
-        float acceleration1 = data.nextInt();
-        float force;
-        force = computeForceFormula(mass, acceleration1);
-        System.out.println("The force is: " + force + " N \n");
-        
-        System.out.println("KINETIC ENERGY: ");
-        System.out.println("Enter mass (kg): ");
-        float mass1 = data.nextInt();
-        System.out.println("Enter speed (m/s): ");
-        float speed = data.nextInt();
-        float kineticEnergy;
-        kineticEnergy = computeKineticEnergyFormula(mass1, speed);
-        System.out.println("The kinetic energy is: " + kineticEnergy + " J");
-        
-        
-
     }
-//Area geometric figures:
+
+    private static void getConeAreaData(Scanner data) {
+        System.out.println("Enter radio (cm): ");
+        float radio1 = data.nextInt();
+        System.out.println("Enter the aperture (cm): ");
+        float aperture = data.nextInt();
+        float coneArea;
+        coneArea = computeConeArea(radio1, aperture);
+        System.out.println("The circle area is: " + coneArea + " cm² \n");
+    }
+
+    private static void getCircleAreaData(Scanner data) {
+        System.out.println("Enter radio (cm): ");
+        float radio = data.nextInt();
+        float circleArea;
+        circleArea = computeCircleArea(radio);
+        System.out.println("The circle area is: " + circleArea + " cm² \n");
+    }
+
+    private static void getRhombusAreaData(Scanner data) {
+        System.out.println("Enter the major diagonal (cm): ");
+        float majorDiagonal = data.nextInt();
+        System.out.println("Enter the minor diagonal (cm): ");
+        float minorDiagonal = data.nextInt();
+        float rhrombusArea;
+        rhrombusArea = computeRhombusArea(majorDiagonal, minorDiagonal);
+        System.out.println("The rhombus area is: " + rhrombusArea + " cm² \n");
+    }
 
     public static float computeRhombusArea(float majorDiagonal, float minorDiagonal) {
         float rhrombusArea = 0.00F;
@@ -84,7 +132,6 @@ public class WS13Task {
         coneArea = pi * radio * (radio + aperture);
         return coneArea;
     }
-//Physics formulas
 
     public static float computePositionFormula(float initialPosition, float time, float acceleration, float initialSpeed) {
         float position = 0.00F;
